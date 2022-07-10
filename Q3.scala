40d67bdeb12a735ae0d52228b95abec6ab0ec254
nadee-darshika <88460737+nadee-darshika@users.noreply.github.com>
2022-07-10 18:37:23 +0530

    Update Q3.scala

--- Q3.scala
+++ Q3.scala
@@ -1,11 +1,28 @@
-val a : Int = 2;
-val b : Int = 3;
-val c : Int = 4;
-val d : Int = 5;
-val k : Float = 4.3f;
+var a : Int = 2;
+var b : Int = 3;
+var c : Int = 4;
+var d : Int = 5;
+var k : Float = 4.3f;
 
-println(--b*a+c*d--);
-println(a++);
-println(-2*(g-k)+c);
-println(c=c++);
-println(c=++c*a++);
+//println(--b*a+c*d--);  =======>>  java answer = 24
+b -= 1;
+println(b*a+c*d);
+d -= 1;
+
+//println(a++);  =======>>  java answer = 2
+println(a);
+a += 1;
+
+//println(-2*(a-k)+c);  =======>>  java answer = 6.6000004
+println(-2*(a-k)+c);
+
+
+//println(c=c++);  =======>> java answer = 4
+println(c);
+
+
+//println(c=++c*a++);  =======>>  java answer = 15
+c+=1;
+println(c*a);                     
+a+=1;
+
