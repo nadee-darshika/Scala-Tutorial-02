81255c3f73f7d5715b097b4ab193a2bb98d83c27
nadee-darshika <88460737+nadee-darshika@users.noreply.github.com>
2022-07-10 10:15:08 +0530

    Create Q3.scala

---
+++ Q3.scala
@@ -0,0 +1,11 @@
+val a : Int = 2;
+val b : Int = 3;
+val c : Int = 4;
+val d : Int = 5;
+val k : Float = 4.3f;
+
+println(--b*a+c*d--);
+println(a++);
+println(-2*(g-k)+c);
+println(c=c++);
+println(c=++c*a++);
