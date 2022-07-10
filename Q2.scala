c8ea8e98044549be5efdb0705556c34d65cf3dda
nadee-darshika <88460737+nadee-darshika@users.noreply.github.com>
2022-07-10 19:04:52 +0530

    Update Q2.scala

--- Q2.scala
+++ Q2.scala
@@ -1,18 +1,15 @@
-val k,i,j : Int = 2;
-val m,n : Int = 5;
-val f : Float = 12.0f;
-val g : Float = 4.0f;
-val c : Char = 'X';
+var k,i,j : Int = 2;
+var m,n : Int = 5;
+var f : Float = 12.0f;
+var g : Float = 4.0f;
+var c : Char = 'X';
 
 k+12*m                // res1: Int = 62
 m/j                   // res2: Int = 2
 n%j                   // res3: Int = 1
 f+10*5+g              // res4: Float = 66.0
 
-++j*n   
-/* <console>:12: error: not found: value ++
-       ++j * n
-       ^
-<console>:12: error: not found: value *
-       ++j * n
-           ^  */
+//++j*n 
+j +=1
+j*n                  // res5: Int = 15
+
